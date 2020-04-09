b4883a75d06c71f9bf1e0b4dc59493a443fbe29c
Patrick <41715581+patrickA25@users.noreply.github.com>
2020-04-09 14:51:29 -0700

    Update Pulling_10_years_of_data.sh
    remove read varname and replaced it with a proper code to have a user inpute.

--- Getting_data/Pulling_10_years_of_data.sh
+++ Getting_data/Pulling_10_years_of_data.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
+wget --wait=2.5 -i 1_Pulling_data/10_year_weather_data_list.txt
 
-wget --wait=2.5 -i 10_year_weather_data_list.txt
+echo "all data has been pulled from FTP"
+read -p "press enter to continue"
 
-read varname
+cd "$(g:/Weather_project/1_Pulling_data)"
+
+#Unzipping files and moving them to raw_data_location
+for file  in $(ls * | grep -i 'csv.gz')
+do
+        echo $file
+        file_name="${file/.gz/}"
+        echo $file_name
+        gunzip -c 1_Pulling_data/$file > 2_Raw_data/$file_name
+done
+
+echo "All weather files have been unzipped"
+read -p " press enter to end the function"
